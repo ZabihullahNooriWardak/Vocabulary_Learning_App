cbc9f70d47996754be7ee46a825cf6904ebe2906
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-05-15 07:16:14 +0430

    create first table

---
+++ expense_tracker/lib/database/db_table.dart
@@ -0,0 +1,9 @@
+import 'package:drift/drift.dart';
+
+class Vocabulary extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get word => text()();
+  TextColumn get definition => text()();
+  TextColumn get exampleSentence => text().nullable()();
+  BoolColumn get mastered => boolean().withDefault(const Constant(false))();
+}
