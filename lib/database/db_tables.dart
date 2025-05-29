0b2b2d29a5e274ea6eedea9f2132716184e88cb7
ZabihullahNooriWardak <zabinoori712@gmail.com>
2025-05-29 07:12:17 +0430

    adds category column

--- lib/database/db_tables.dart
+++ lib/database/db_tables.dart
@@ -1,11 +1,18 @@
 import 'package:drift/drift.dart';
 
-
 class Vocabulary extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get word => text()();
   TextColumn get definition => text()();
   TextColumn get exampleSentence => text().nullable()();
   BoolColumn get mastered => boolean().withDefault(const Constant(false))();
+  IntColumn get categoryId =>
+      integer().references(VCategory, #id, onDelete: KeyAction.restrict)();
+  DateTimeColumn get createdAt =>
+      dateTime().nullable().clientDefault(() => DateTime.now())();
+}
 
+class VCategory extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().unique()();
 }
